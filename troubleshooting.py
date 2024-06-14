a4b56b935118e822881d42afa2d5a0d081c667d1
Coda West <codawestdunn@codas-air.devices.brown.edu>
2024-06-14 14:21:24 -0400

    updating the file

--- troubleshooting.py
+++ troubleshooting.py
@@ -34,10 +34,9 @@ def get_cell_density(numcells, area):
 
 cell_density = []
 for k in range(1,4):
-    area = dff.iloc[k]["area"]
+    area = df.iloc[k]["area"]
     numcells = df.iloc[k]["numcells"]
     cell_density.append( get_cell_density(area,numcells))
 
 df["cell_density"] = cell_density
-
 
